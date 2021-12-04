477fd3aa0d4bbd9104ca191fba535f74f3524df5
rnshch <rn.radouane@gmail.com>
2021-12-04 19:03:06 +0100

    challeng2 resolved

--- Chapters/ch2/test_seasons.py
+++ Chapters/ch2/test_seasons.py
@@ -1,5 +1,8 @@
 from Chapters.ch2.seasons import different_season
 
 
-def test_the_input_winter():
-    assert different_season("winter") == "snow"
+def test_the_input_winter(monkeypatch):
+    input1 = "winter"
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda x: input1)
+        assert different_season() == "snow"
