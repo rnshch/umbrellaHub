1a0b5b8fb913c6a536d3c574144c41acb43392e9
rnshch <rn.radouane@gmail.com>
2021-12-04 03:12:59 +0100

    challenge2

---
+++ Chapters/ch1/challenge2.py
@@ -0,0 +1,5 @@
+season = input('What is the season?')
+adjective = input('Enter an Adjective')
+number = input('Enter a number')
+
+print(f'On a {adjective} {season}, i drink {number} cups of juice!!!!! ')
